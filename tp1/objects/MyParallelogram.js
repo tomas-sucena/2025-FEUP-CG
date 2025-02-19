c2b853a0aea08180165d80cebd0ab1bb95742d14
Tomás Sucena Lopes <tomas21sucena@gmail.com>
2025-02-19 12:27:07 +0000

    refactor(tp1): reduce parallelogram vertices

--- tp1/objects/MyParallelogram.js
+++ tp1/objects/MyParallelogram.js
@@ -13,26 +13,20 @@ export class MyParallelogram extends CGFobject {
     initBuffers() {
         this.vertices = [
             0, 0, 0, //0
-            1, 0, 0, //1
-            2, 0, 0, //2
-            3, 1, 0, //3
-            2, 1, 0, //4
-            1, 1, 0, //5
+            2, 0, 0, //1
+            3, 1, 0, //2
+            1, 1, 0, //3
         ];
 
         //Counter-clockwise reference of vertices
         this.indices = [
             // front
-            0, 1, 5,
-            1, 2, 5,
-            2, 4, 5,
-            2, 3, 4,
+            0, 1, 3,
+            1, 2, 3,
 
             // back
-            5, 1, 0,
-            5, 2, 1,
-            5, 4, 2,
-            4, 3, 2,
+            3, 1, 0,
+            3, 2, 1,
         ];
 
         //The defined indices (and corresponding vertices)
