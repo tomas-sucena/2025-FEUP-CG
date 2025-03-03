c65f7db46b5f4a1782d2db3fc6394cd5bdd3c7ef
rita cachaldora <up202108798@up.pt>
2025-03-03 22:50:10 +0000

    myunitcube not working well

---
+++ tp2/objects/MyUnitCubeQuad.js
@@ -0,0 +1,54 @@
+import { MyObject } from './MyObject.js';
+import { MyQuad } from './MyQuad.js';
+
+/**
+ * MyUnitCubeQuad
+ * @constructor
+ * @param scene - Reference to MyScene object
+ */
+export class MyUnitCubeQuad extends MyObject {
+    constructor(scene) {
+        super(scene);
+
+        this.quad = new MyQuad();
+    }
+
+    render() {
+        // front square
+        this.quad
+            .translate(0, 0, 0.5)
+            .display()
+
+        // back square
+        this.quad
+            .translate(0, 0, -0.5)
+            .display()        
+
+        // right square
+        this.quad
+            .rotate(Math.PI/2, 0, 1, 0)
+            .translate(0.5, 0, 0)
+            .display()       
+            
+        // left square
+        this.quad
+            .rotate(-Math.PI/2, 0, 1, 0)
+            .translate(-0.5, 0, 0)
+            .display()  
+
+        // top square
+        this.quad
+            .rotate(Math.PI/2, 1, 0, 0)
+            .translate(0, 0.5, 0)
+            .display()    
+
+        // bottom square
+        this.quad
+            .rotate(-Math.PI/2, 1, 0, 0)
+            .translate(0, -0.5, 0)
+            .display()   
+            
+    }
+
+
+}
