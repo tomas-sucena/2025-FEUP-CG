0fb67000bb246548c9efe80f2cbacaf8e0cb2f40
Tomás Sucena Lopes <tomas21sucena@gmail.com>
2025-03-19 12:11:01 +0000

    fix(tp3): fix tangram materials

--- tp3/objects/MyTangram.js
+++ tp3/objects/MyTangram.js
@@ -28,44 +28,44 @@ export class MyTangram extends MyObject {
     initMaterials() {
         this.greenMaterial = new CGFappearance(this.scene);
         this.greenMaterial.setAmbient(0, 1, 0, 1);
-        this.greenMaterial.setSpecular(0, 1, 0, 1);
         this.greenMaterial.setDiffuse(0, 1, 0, 1);
+        this.greenMaterial.setSpecular(0.8, 0.8, 0.8, 1);
         this.greenMaterial.setShininess(10.0);
 
         this.blueMaterial = new CGFappearance(this.scene);
         this.blueMaterial.setAmbient(0, 0.5, 1, 1);
-        this.blueMaterial.setSpecular(0, 0.5, 1, 1);
         this.blueMaterial.setDiffuse(0, 0.5, 1, 1);
+        this.blueMaterial.setSpecular(0.8, 0.8, 0.8, 1);
         this.blueMaterial.setShininess(10.0);
 
         this.redMaterial = new CGFappearance(this.scene);
         this.redMaterial.setAmbient(1, 27 / 255, 27 / 255, 1);
-        this.redMaterial.setSpecular(1, 27 / 255, 27 / 255, 1);
         this.redMaterial.setDiffuse(1, 27 / 255, 27 / 255, 1);
+        this.redMaterial.setSpecular(0.8, 0.8, 0.8, 1);
         this.redMaterial.setShininess(10.0);
 
         this.orangeMaterial = new CGFappearance(this.scene);
         this.orangeMaterial.setAmbient(1, 0.5, 0, 1);
-        this.orangeMaterial.setSpecular(1, 0.5, 0, 1);
         this.orangeMaterial.setDiffuse(1, 0.5, 0, 1);
+        this.orangeMaterial.setSpecular(0.8, 0.8, 0.8, 1);
         this.orangeMaterial.setShininess(10.0);
 
         this.yellowMaterial = new CGFappearance(this.scene);
-        this.yellowMaterial.setAmbient(1, 1, 0, 1);
         this.yellowMaterial.setSpecular(1, 1, 0, 1);
         this.yellowMaterial.setDiffuse(1, 1, 0, 1);
+        this.yellowMaterial.setAmbient(0.8, 0.8, 0.8, 1);
         this.yellowMaterial.setShininess(10.0);   
         
         this.purpleMaterial = new CGFappearance(this.scene);
         this.purpleMaterial.setAmbient(150 / 255, 80 / 255, 190 / 255, 1);
-        this.purpleMaterial.setSpecular(150 / 255, 80 / 255, 190 / 255, 1);
         this.purpleMaterial.setDiffuse(150 / 255, 80 / 255, 190 / 255, 1);
+        this.purpleMaterial.setSpecular(0.8, 0.8, 0.8, 1);
         this.purpleMaterial.setShininess(10.0);   
 
         this.pinkMaterial = new CGFappearance(this.scene);
         this.pinkMaterial.setAmbient(1, 0.5, 207 / 255, 1);
-        this.pinkMaterial.setSpecular(1, 0.5, 207 / 255, 1);
         this.pinkMaterial.setDiffuse(1, 0.5, 207 / 255, 1);
+        this.pinkMaterial.setSpecular(0.8, 0.8, 0.8, 1);
         this.pinkMaterial.setShininess(10.0);   
     }
 
