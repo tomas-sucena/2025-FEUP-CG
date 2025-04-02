2a1fb8e2925b7d86aecead62193839f5e7455475
Tomás Sucena Lopes <tomas21sucena@gmail.com>
2025-04-02 11:29:27 +0100

    feat(proj): initialize scene

---
+++ project/MyScene.js
@@ -0,0 +1,97 @@
+import {
+    CGFscene,
+    CGFcamera,
+    CGFaxis,
+    CGFappearance,
+    CGFtexture,
+} from '../lib/CGF.js';
+
+import { MySphere } from './objects/solids/MySphere.js';
+
+/**
+ * MyScene
+ * @constructor
+ */
+export class MyScene extends CGFscene {
+    constructor() {
+        super();
+    }
+
+    init(application) {
+        super.init(application);
+        this.initCameras();
+        this.initLights();
+
+        //Background color
+        this.gl.clearColor(0.0, 0.0, 0.0, 1.0);
+
+        this.gl.clearDepth(100.0);
+        this.gl.enable(this.gl.DEPTH_TEST);
+        this.gl.enable(this.gl.CULL_FACE);
+        this.gl.depthFunc(this.gl.LEQUAL);
+        this.enableTextures(true);
+
+        //Initialize scene objects
+        this.axis = new CGFaxis(this);
+        this.objects = {
+            'Sphere': new MySphere(this, 3, 3),
+        };
+
+        //-------Objects connected to MyInterface
+        this.displayAxis = true;
+        this.displayNormals = false;
+        this.selectedObject = 'Sphere';
+        this.scaleFactor = 1;
+    }
+
+    initLights() {
+        this.lights[0].setPosition(5, 2, 5, 1);
+        this.lights[0].setDiffuse(1.0, 1.0, 1.0, 1.0);
+        this.lights[0].enable();
+        this.lights[0].update();
+    }
+
+    initCameras() {
+        this.camera = new CGFcamera(
+            0.4,
+            0.1,
+            500,
+            vec3.fromValues(15, 15, 15),
+            vec3.fromValues(0, 0, 0),
+        );
+    }
+
+    // updates the selected object's wireframe mode
+    /*onWireframeChanged(v) {
+        if (v) this.objects[this.selectedObject].setLineMode();
+        else this.objects[this.selectedObject].setFillMode();
+    }*/
+
+    display() {
+        // ---- BEGIN Background, camera and axis setup
+        // Clear image and depth buffer everytime we update the scene
+        this.gl.viewport(0, 0, this.gl.canvas.width, this.gl.canvas.height);
+        this.gl.clear(this.gl.COLOR_BUFFER_BIT | this.gl.DEPTH_BUFFER_BIT);
+        // Initialize Model-View matrix as identity (no transformation
+        this.updateProjectionMatrix();
+        this.loadIdentity();
+        // Apply transformations corresponding to the camera position relative to the origin
+        this.applyViewMatrix();
+
+        // Draw axis
+        if (this.displayAxis) this.axis.display();
+
+        this.scale(this.scaleFactor, this.scaleFactor, this.scaleFactor);
+
+        // ---- BEGIN Primitive drawing section
+        const object = this.objects[this.selectedObject];
+
+        (this.displayNormals)
+            ? object.enableNormalViz()
+            : object.disableNormalViz();
+
+        object.display();
+
+        // ---- END Primitive drawing section
+    }
+}
---
+++ project/objects/solids/MySphere.js
@@ -0,0 +1,71 @@
+import { MyObject } from '../MyObject.js';
+
+/**
+ * MyCylinder
+ * @constructor
+ * @param scene - Reference to MyScene object
+ * @param slices - number of divisions around the Z axis
+ * @param stacks - number of divisions along the Z axis
+ */
+export class MySphere extends MyObject {
+    constructor(scene, slices, stacks) {
+        super(scene);
+
+        this.slices = slices;
+        this.stacks = stacks;
+        this.initBuffers();
+    }
+
+    initBuffers() {
+        this.vertices = [];
+        this.indices = [];
+        this.normals = [];
+
+        const angOffset = (2 * Math.PI) / this.slices;
+        const zOffset = 1 / this.stacks;
+
+        // define the slices
+        for (let slice = 0; slice < this.slices; ++slice) {
+            const ang = slice * angOffset;
+            const sa = Math.sin(ang);
+            const ca = Math.cos(ang);
+
+            // define the stacks
+            for (let stack = 0; stack <= this.stacks; ++stack) {
+                // define the indices (except for the last vertex of each stack)
+                if (stack < this.stacks) {
+                    const index = this.vertices.length / 3;
+                    const indexNextSlice = (slice + 1 < this.slices)
+                        ? index + this.stacks + 1
+                        : stack;
+                    
+                    this.indices.push(
+                        index, indexNextSlice, index + 1,
+                        indexNextSlice, indexNextSlice + 1, index + 1,
+                    );
+                }
+
+                // define the vertices
+                this.vertices.push(ca, sa, stack * zOffset);
+
+                // define the normals
+                this.normals.push(ca, sa, 0);
+            }
+        }
+
+        this.primitiveType = this.scene.gl.TRIANGLES;
+        this.initGLBuffers();
+    }
+
+    /**
+     * Called when user interacts with GUI to change object's complexity.
+     * @param {integer} complexity - changes number of slices
+     */
+    updateBuffers(complexity) {
+        this.slices = Math.max(3, Math.round(16 * complexity));
+
+        // reinitialize buffers
+        this.initBuffers();
+        this.initNormalVizBuffers();
+    }
+}
