6f7c81a79a7687e703c5954e9b837ce83e142453
Tomás Sucena Lopes <tomas21sucena@gmail.com>
2025-04-11 17:44:02 +0100

    feat(proj): simplify panorama

--- project/MyScene.js
+++ project/MyScene.js
@@ -35,7 +35,6 @@ export class MyScene extends CGFscene {
         //Initialize scene objects
         this.axis = new CGFaxis(this);
         this.skybox = new MyPanorama(this, {
-            type: MyPanorama.type.CYLINDER,
             position: this.camera.position,
             scaleFactor: 200,
             texture: './assets/earth.jpg',
--- project/objects/MyPanorama.js
+++ project/objects/MyPanorama.js
@@ -1,16 +1,10 @@
 import { MyObject } from './MyObject.js';
-import { MyCylinder } from './solids/MyCylinder.js';
 import { MySphere } from './solids/MySphere.js';
 
 /**
  * A 360° scenic view.
  */
 export class MyPanorama extends MyObject {
-    static type = Object.freeze({
-        SPHERE: MySphere,
-        CYLINDER: MyCylinder,
-    });
-
     /**
      * Initializes the panorama.
      * @param { MyScene } scene a reference to the MyScene object
@@ -19,14 +13,13 @@ export class MyPanorama extends MyObject {
     constructor(scene, config) {
         super(scene, config);
 
-        /** The object that constitutes the panorama */
-        this.shape = new config.type(scene, {
+        /** The sphere that constitutes the panorama */
+        this.sphere = new MySphere(scene, {
             slices: 50,
             stacks: 25,
             texture: config.texture,
             invert: !config.invert,
         });
-
         /** The coordinates where the panorama will be drawn */
         this.position = config.position;
         /** The scale that will be applied to the box */
@@ -34,7 +27,7 @@ export class MyPanorama extends MyObject {
     }
 
     render() {
-        this.shape
+        this.sphere
             .scale(this.scaleFactor, this.scaleFactor, this.scaleFactor)
             .translate(...this.position)
             .display();
