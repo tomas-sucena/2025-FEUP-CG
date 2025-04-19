dd0f8c718572d51c6fb488bcd5a8eca7e35e1100
Tomás Sucena Lopes <tomas21sucena@gmail.com>
2025-04-19 10:11:18 +0100

    feat(proj): add building ceiling

--- project/objects/building/MyModule.js
+++ project/objects/building/MyModule.js
@@ -21,14 +21,10 @@ export class MyModule extends MyObject {
         this.initGeometry(config);
     }
 
-    #addWalls() {}
-
-    initBuffers() {
-        this.vertices = [];
-        this.indices = [];
-        this.normals = [];
-        this.texCoords = [];
-
+    /**
+     * Defines the module's walls by updating the object's buffers.
+     */
+    #addWalls() {
         const walls = 3 + this.isMainModule;
         const xOffset = this.width / this.windows;
         const yOffset = this.height / this.floors;
@@ -81,7 +77,56 @@ export class MyModule extends MyObject {
                 }
             }
         }
+    }
 
+    /**
+     * Defines the module's ceiling by updating the object's buffers.
+     */
+    #addCeiling() {
+        const xOffset = this.width / this.windows;
+        const zOffset = this.depth / this.windows;
+
+        for (let i = 0; i <= this.windows; ++i) {
+            const z = -this.depth / 2 + i * zOffset;
+
+            for (let j = 0; j <= this.windows; ++j) {
+                const x = -this.width / 2 + j * xOffset;
+
+                // define the indices
+                if (i < this.windows && j < this.windows) {
+                    const index = this.vertices.length / 3;
+                    const indexNextFloor = index + this.windows + 1;
+
+                    // prettier-ignore
+                    this.indices.push(
+                        index, indexNextFloor, index + 1,
+                        index + 1, indexNextFloor, indexNextFloor + 1,
+                    );
+                }
+
+                // define the vertices
+                this.vertices.push(x, this.height, z);
+
+                // define the normals
+                this.normals.push(0, 1, 0);
+
+                // define the texture coordinates
+                this.texCoords.push(j, i);
+            }
+        }
+    }
+
+    /**
+     * Initializes the module's vertices, indices, normals, and texture coordinates.
+     */
+    initBuffers() {
+        this.vertices = [];
+        this.indices = [];
+        this.normals = [];
+        this.texCoords = [];
+
+        this.#addWalls();
+        this.#addCeiling();
         super.initBuffers();
     }
 }
