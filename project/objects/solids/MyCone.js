88a947c56d18de29d5a6cf1f4deb4e340afea2e7
rita cachaldora <up202108798@up.pt>
2025-04-23 12:04:11 +0100

    mycone

---
+++ project/objects/solids/MyCone.js
@@ -0,0 +1,59 @@
+import { MyObject } from '../MyObject.js';
+
+/**
+ * A cone.
+ */
+export class MyCone extends MyObject {
+    /**
+     * Initializes the cone.
+     * @param { MyScene } scene the scene the object will be displayed in
+     * @param { Object } config the object configuration
+     */
+    constructor({
+        scene,
+        radius,
+        height,
+        slices,
+        stacks,
+        inverted,
+        material,
+        texture,
+    }) {
+        super(scene);
+
+        /** The width of the cone */
+        this.radius = radius ?? 1;
+        /** The height of the cone */
+        this.height = height ?? 1;
+        /** The number of divisions around the Y-axis */
+        this.slices = slices ?? 16;
+        /** The number of divisions along the Y-axis*/
+        this.stacks = stacks ?? 1;
+
+        this.initGeometry({ inverted, material, texture });
+    }
+
+    initBuffers() {
+        this.vertices = [];
+        this.indices = [];
+        this.normals = [];
+        this.texCoords = [];
+
+        const deltaAng = (-2 * Math.PI) / this.slices;
+        const deltaY = this.height / this.stacks;
+
+        // define the stacks
+        for (let stack = 0; stack <= this.stacks; ++stack) {
+            const y = stack * deltaY;
+
+            // define the slices
+            for (let slice = 0; slice <= this.slices; ++slice) {
+                const ang = slice * deltaAng;
+                const ca = Math.cos(ang);
+                const sa = Math.sin(ang);
+
+                this.vertices.push(ca, y, sa);
+            }
+        }
+    }
+}
