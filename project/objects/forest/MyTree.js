c8c95e41e2b19912ffe237050411a0a7a4cb2510
Tomás Sucena Lopes <tomas21sucena@gmail.com>
2025-04-29 16:56:55 +0100

    refactor(proj): improve tree

--- project/objects/forest/MyTree.js
+++ project/objects/forest/MyTree.js
@@ -3,7 +3,7 @@ import { MyCone } from '../solids/MyCone.js';
 import { MyPyramid } from '../solids/MyPyramid.js';
 
 export class MyTree extends MyObject {
-    constructor({ scene, radius, height, stacks, colors, textures }) {
+    constructor({ scene, radius, height, stacks, slices, colors, textures }) {
         super(scene);
         const crownHeight = 0.8 * height;
 
@@ -36,24 +36,39 @@ export class MyTree extends MyObject {
             scene,
             radius: 3 * radius,
             height: crownHeight,
-            slices: 6,
+            slices: slices ?? 6,
+            texture: textures?.crown,
         });
     }
 
+    /**
+     * Returns the radius of the tree, which corresponds to the radius of its crown.
+     */
+    get radius() {
+        return this.crown.radius;
+    }
+
+    /**
+     * Displays the geometry of the tree.
+     */
     render() {
-        const deltaScaleFactor = 1 / this.stacks;
+        const deltaHeightFactor = 1 / this.stacks;
+        const deltaRadiusFactor = 0.6 / this.stacks;
         const deltaY = this.crown.height / this.stacks;
 
+        const visibleLogHeight = 0.2 * this.height;
+
         // display the trunk
-        this.trunk.translate(0, this.crown.height - this.height, 0).display();
+        this.trunk.translate(0, -visibleLogHeight, 0).display();
 
         // display the crown
         for (let stack = 0; stack < this.stacks; ++stack) {
-            const scaleFactor = 1 - stack * deltaScaleFactor;
-            const Y = 0.2 * this.height + stack * deltaY;
+            const radiusFactor = 1 - stack * deltaRadiusFactor;
+            const heightFactor = 1 - stack * deltaHeightFactor;
+            const Y = visibleLogHeight + stack * deltaY;
 
             this.crown
-                .scale(scaleFactor, scaleFactor, scaleFactor)
+                .scale(radiusFactor, heightFactor, radiusFactor)
                 .translate(0, Y, 0)
                 .display();
         }
